0595380c80a217b415cb49528bb5d2dcc6c3b1ea
Naufal Fadhiil <naufalfadhiil2206@gmail.com>
2025-07-25 14:47:35 +0700

    Fixed: handle overflow startpage and widget structure

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -8,89 +8,77 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return Column(
-              // LOGO
-              children: [
-                Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 60),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 40),
-                          child: Image.asset(
-                            'lib/images/logo_start.png',
-                            width: 200,
-                            height: 250,
-                          ),
-                        ),
-                      ],
-                    ),
+        child: SingleChildScrollView(
+          child: Column(
+            // LOGO
+            children: [
+              const SizedBox(height: 60),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40),
+                child: Image.asset(
+                  'lib/images/logo_start.png',
+                  width: 200,
+                  height: 250,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(
+                  bottom: 10.0,
+                  top: 100.0,
+                  left: 13.0,
+                  right: 13.0,
+                ),
+                child: Text(
+                  'Welcome to Movie List',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Poppins',
                   ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 10.0,
-                    top: 100.0,
-                    left: 13.0,
-                    right: 13.0,
-                  ),
-                  child: Text(
-                    'Welcome to Movie List',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'Poppins',
-                    ),
-                  ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Text(
+                  'Find your favorite movies and series',
+                  style: TextStyle(fontSize: 20, fontFamily: 'Poppins'),
                 ),
-                Container(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: Text(
-                    'Find your favorite movies and series',
-                    style: TextStyle(fontSize: 20, fontFamily: 'Poppins'),
-                  ),
-                ),
+              ),
 
-                const SizedBox(height: 60),
+              const SizedBox(height: 60),
 
-                //Button Get Started
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const HomePage(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      width: 250,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Color(0xFF2b70a8),
-                      ),
-                      padding: EdgeInsets.all(25.0),
-                      child: const Text(
-                        'Get Started',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontFamily: 'Poppins',
-                        ),
+              //Button Get Started
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  },
+                  child: Container(
+                    width: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFF2b70a8),
+                    ),
+                    padding: EdgeInsets.all(25.0),
+                    child: const Text(
+                      'Get Started',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
                       ),
                     ),
-                  ),      
+                  ),
                 ),
-              ],
-            );
-          },
+              ),
+            ],
+          ),
         ),
       ),
     );
